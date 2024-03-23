2885016952dff1926b95144717d190f6e5c7478d
Swaen Boutu <swaen@boutu.fr>
2024-03-23 16:48:48 +0100

    Adding an animation when the anagram is display

--- src/anagramDisplay.js
+++ src/anagramDisplay.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function Anagram({anagram}) {
+    const arrayDataItems = Array.from(anagram).map(element => 
+        <span>{element}</span>
+        )
+
     return (
+        <>
         <div>
-            <h2 className="anagram">{anagram}</h2>
+            <h2 className="anagram animate">        
+                {arrayDataItems}
+            </h2>
         </div>
+        </>
     );
 }
 
